e246eac4a055fbbfdb165660f709c0ce3ddc88cd
Juanjo Labella <jjlabella@gmail.com>
2015-11-25 23:55:57 +0100

    Initial push.

---
+++ CropRoundedImageView/CropRoundedImageView.swift
@@ -0,0 +1,9 @@
+//
+//  CropRoundedImageView.swift
+//  CropRoundedImageView
+//
+//  Created by Juan Jose Labella on 25/11/15.
+//  Copyright (c) 2015 jjlabella. All rights reserved.
+//
+
+import Foundation
